2e87af8e98fc4d302bbb24fb7297fbab6075ff0c
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-01-21 05:21:34 +0100

    add provisioning script

---
+++ provision.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash 
+
+# options
+BRANCH="master"
+REPO="https://github.com/awaelchli/ansible.git"
+VAULT_KEY_FILE="$HOME/.vault-key"
+PRECMD="sudo systemd-inhibit --who='ansible-pull' --why='provisioning'"
+
+# install ansible if necessary
+apt install -y ansible
+
+# run ansible-pull command
+if [ $# -eq 0 ]
+    then
+        $PRECMD sudo ansible-pull --clean --vault-password-file=$VAULT_KEY_FILE -U $REPO -C $BRANCH 2>&1
+    else
+        $PRECMD sudo ansible-pull --clean --vault-password-file=$VAULT_KEY_FILE -U $REPO -C $BRANCH --tags $1 2>&1
+fi
